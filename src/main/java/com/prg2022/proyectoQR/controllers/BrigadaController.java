6340b1c843940dff4b0000b0d332a0031f6c401a
James <extremo_ve@hotmail.com>
2022-05-09 00:15:24 +0200

    Brigadas

--- src/main/java/com/prg2022/proyectoQR/controllers/BrigadaController.java
+++ src/main/java/com/prg2022/proyectoQR/controllers/BrigadaController.java
@@ -3,15 +3,16 @@ package com.prg2022.proyectoQR.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(value = {"/brigada"})
 
 public class BrigadaController {
     @GetMapping("")
-    @ResponseBody
-    public String listado() {
-        return "Public Content.";
+
+    public ModelAndView getBrigadas(ModelAndView modelAndView) {
+        modelAndView.setViewName("brigada");
+        return modelAndView;
     }
 }
