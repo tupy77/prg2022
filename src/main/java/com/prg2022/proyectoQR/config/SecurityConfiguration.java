da6f9ead299162b50eb4df826ad370b3a2daaa55
james <james@local>
2022-05-11 10:36:53 +0200

    login de usuario

--- src/main/java/com/prg2022/proyectoQR/config/SecurityConfiguration.java
+++ src/main/java/com/prg2022/proyectoQR/config/SecurityConfiguration.java
@@ -1,12 +1,8 @@
 package com.prg2022.proyectoQR.config;
 
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-
-
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -14,35 +10,28 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 		/*
-        http.authorizeRequests()
-                .antMatchers("/h2-console/**").permitAll()
-				.antMatchers("/").hasRole("Admin");
         http.csrf().disable();
         http.headers().frameOptions().disable();
-    }*/
-
-
-
+    	*/
 		http
 				.authorizeRequests((authorize) -> authorize
-					.antMatchers("/css/**", "/index", "/js/**", "/").permitAll()
-					.antMatchers("/admin").hasRole("Admin")
-					.antMatchers("/brigada").hasAnyRole("Usuario", "Admin")
+					.antMatchers("/css/**", "/index", "/js/**", "/login*","/test-*").permitAll()
+					.antMatchers("/admin/**").hasRole("Admin")
+					.antMatchers("/brigada/**").hasAnyRole("Usuario", "Admin")
+					.anyRequest().authenticated()
 				)
 				.formLogin((formLogin) -> formLogin
 					.loginPage("/login")
-					.failureUrl("/login-error")
+					.loginProcessingUrl("/login-user")
+					.defaultSuccessUrl("/", true)
+					.failureUrl("/login?error=true")
 				);
 	}
-/*
-	@Bean
-	public UserDetailsService userDetailsService() {
-		UserDetails userDetails = Alumno.withDefaultPasswordEncoder()
-				.username("user")
-				.password("password")
-				.roles("USER")
-				.build();
-		return new InMemoryUserDetailsManager(userDetails);
-	}*/
-
 }
+
+		/*
+      .logout()
+      .logoutUrl("/perform_logout")
+      .deleteCookies("JSESSIONID")
+      .logoutSuccessHandler(logoutSuccessHandler());
+	  */
